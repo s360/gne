06e371a8d0110654242a0f7cb3cc9c3e68c6fb9c
komar <komar@upwardstech.com>
2015-06-12 18:06:13 +0700

    edit html.tpl.php

--- themes/bootstrap/theme/system/html.tpl.php
+++ themes/bootstrap/theme/system/html.tpl.php
@@ -51,6 +51,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
+
+    <!--[if IE]>
+    <link rel="stylesheet" type="text/css" href="/<?php echo $directory; ?>/css/style-ie.css" />
+    <![endif]-->
+
   <?php print $styles; ?>
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
