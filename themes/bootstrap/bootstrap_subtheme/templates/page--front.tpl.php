22902a9e0b139afde3eb961bd7f43e3a07c83581
Abraham Godong <abe@godong.or.id>
2015-06-11 20:54:05 -0700

    Initial theme commit
    Initial theme commit

---
+++ themes/bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.tpl.php template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see bootstrap_preprocess_page()
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see bootstrap_process_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup themeable
+ */
+?>
+<?php
+drupal_add_css($directory.'/js/slick/slick.css', array('group' => CSS_THEME, 'every_page' => FALSE));
+drupal_add_css($directory.'/js/slick/slick-theme.css', array('group' => CSS_THEME, 'every_page' => FALSE));
+?>
+<div id="homepage">
+<header id="header" role="banner" class="">
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="navbar-header">
+        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+           <?php print '<img src="'.base_path() . path_to_theme() .'/images/gne_logo.png">'; ?>
+        </a>
+
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+
+    </div>
+       <div class="pull-right">
+           <a class="btn-donate" href="#">Donate</a>
+           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" style="display: block">
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+           </button>
+           <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+               <div class="top-navigation">
+                   <nav role="navigation">
+                       <?php if (!empty($page['navigation'])): ?>
+                           <div class="slider-nav">
+                               <?php print render($page['navigation']); ?>
+                           </div>
+                       <?php endif; ?>
+
+                   </nav>
+               </div>
+           <?php endif; ?>
+       </div>
+
+  </div>
+</header>
+<div class="main-container">
+        <div class="">
+            <section class="wrap-homepage">
+                <?php print render($page['content']); ?>
+            </section>
+        </div>
+</div>
+
+
+<footer id="footer">
+  <?php //print render($page['footer']); ?>
+    <div class="social pull-left"></div>
+    <div class="copyright pull-right">
+        <ul>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">&copy; 2015 GreatNationsEat.org</a></li>
+        </ul>
+    </div>
+</footer>
+
+</div>
---
+++ themes/bootstrap/js/custom.js
@@ -0,0 +1,149 @@
+/**
+ * Custom Javascript for APATT
+ */
+
+'use strict';
+
+jQuery(document).ready(function() {
+
+    jQuery.fn.reverse = [].reverse;
+
+    jQuery.fn.sliderPage = function() {
+
+        var container = 'div#scrolling-page-container';
+        var next_btn = '#next-scrolling-page';
+        var prev_btn = '#prev-scrolling-page';
+        var total_row = this.find(container).children().length;
+        var $children_row = this.find(container).children();
+
+        this.find(container).children().css({
+            display: 'none',
+            opacity: 1
+        });
+
+        this.find(container).children().first().css({
+            display: 'block',
+            opacity: 1
+        }).addClass('active').addClass('first');
+
+        this.find(container).children().last().addClass('last');
+
+
+
+        jQuery(next_btn).on('click', function(e) {
+
+            var found = false;
+            var ok = false;
+
+            jQuery(container).children().each(function(index) {
+
+                if(found == true)
+                {
+                    jQuery(this).addClass('active').css({
+                        display: 'block',
+                        opacity: 1
+                    });
+
+                    found = false;
+                    ok = true;
+
+                }
+
+                if(jQuery(this).hasClass('active') && ok == false && !jQuery(this).hasClass('last'))
+                {
+                    jQuery(this).removeClass('active').css({
+                        display: 'none',
+                        opacity: 0
+                    });
+
+                    found = true;
+
+                }
+
+            });
+
+        });
+
+
+
+        jQuery(prev_btn).on('click', function(e) {
+
+            var found = false;
+            var ok = false;
+
+            jQuery(container).children().reverse().each(function(index) {
+
+                if(found == true)
+                {
+                    jQuery(this).addClass('active').css({
+                        display: 'block',
+                        opacity: 1
+                    });
+
+                    found = false;
+                    ok = true;
+
+                }
+
+                if(jQuery(this).hasClass('active') && ok == false && !jQuery(this).hasClass('first'))
+                {
+                    jQuery(this).removeClass('active').css({
+                        display: 'none',
+                        opacity: 1
+                    });
+
+                    found = true;
+
+                }
+
+            });
+
+        });
+
+        return this;
+
+    };
+
+    jQuery('#scrolling-page').sliderPage();
+
+    //homepage fullpag
+    var toolbar = 64;
+    var menu = 51;
+    var window_height = jQuery(window).height();
+    var window_width =jQuery(window).width();
+    var window_height_2 =jQuery(window).height()-toolbar;
+
+    if(jQuery('body').hasClass('toolbar-drawer')){
+        jQuery("#homepage").css({"height": window_height_2});
+        jQuery("#container-slide-home").css({"height": window_height_2});
+        jQuery(".node-slider-page").css({"height": window_height_2});
+    }else{
+        jQuery("#homepage").css({"height": window_height});
+        jQuery("#container-slide-home").css({"height": window_height});
+        jQuery(".node-slider-page").css({"height": window_height});
+    }
+
+    // homepage carausel
+    jQuery('#container-slide-home').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-nav .view-content'
+
+    });
+
+    jQuery('.slider-nav .view-content').slick({
+        slidesToShow: 20,
+        slidesToScroll: 20,
+        asNavFor: '#container-slide-home',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+
+    jQuery( ".navbar-toggle" ).click(function() {
+         jQuery('.top-navigation').toggleClass('in');
+    });
+
+    jQuery(".slider-nav .view-content a").removeAttr("href").css("cursor","pointer");
+});
