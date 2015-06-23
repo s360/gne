4bde0be8cc55a758c3ed8818f1b61d83ecbb3bfd
komar <komar@upwardstech.com>
2015-06-23 10:33:47 +0700

    change favicon

--- themes/bootstrap/theme/system/html.tpl.php
+++ themes/bootstrap/theme/system/html.tpl.php
@@ -51,6 +51,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="apple-mobile-web-app-capable" content="yes">
+  <link rel="apple-touch-icon" sizes="57x57" href="/sites/all/themes/bootstrap/images/favicon/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/sites/all/themes/bootstrap/images/favicon/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/sites/all/themes/bootstrap/images/favicon/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/sites/all/themes/bootstrap/images/favicon/apple-touch-icon-76x76.png">
+  <link rel="icon" type="image/png" href="/sites/all/themes/bootstrap/images/favicon/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/sites/all/themes/bootstrap/images/favicon/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="/sites/all/themes/bootstrap/images/favicon/favicon-16x16.png" sizes="16x16">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
 
