dfa98421289dbb2527b7a49f519000b9736db6a4
Kyle <kyle.warbis@gmail.com>
2015-06-23 13:16:32 -0700

    remove .00 from donate money

--- themes/bootstrap/js/convio-start.js
+++ themes/bootstrap/js/convio-start.js
@@ -25,6 +25,7 @@
                         for (var i = 0; i < data.getDonationFormInfoResponse.donationLevels.donationLevel.length; i++) {
                             var donationLevel = data.getDonationFormInfoResponse.donationLevels.donationLevel[i];
                             var donateAmount = donationLevel.amount.formatted;
+
                             if (donationLevel.userSpecified === "true") {
                                 html += '<div class="radio-wrap wrap-amount">' +
                                 '<input type="radio" id="level-other" name="level_id" value="' + donationLevel.level_id + '">' +
@@ -38,7 +39,7 @@
                                 html += '<div class="radio-wrap wrap-amount">' +
                                 '<input type="radio" id="'+ donationLevel.level_id +'" name="level_id" value="' + donationLevel.level_id + '"> ' +
                                 '<label class="donation_amount" for="'+ donationLevel.level_id +'" >' +
-                                donateAmount.substr(0, donationLevel.amount.length-3) +
+                                donateAmount.substr(0, donateAmount.length-3) +
                                 '</label>' +
                                 '</div>';
                             }
