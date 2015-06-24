f9cad94aba24a8664520f5b88ee1b4f522fadf61
Kyle <kyle.warbis@gmail.com>
2015-06-24 15:54:24 -0700

    donate validation

--- themes/bootstrap/js/donate.js
+++ themes/bootstrap/js/donate.js
@@ -28,18 +28,18 @@ jQuery(document).ready(function($) {
             $target.find('input:eq(0)').focus();
         }
     });
-
+      // email = curInputs[6].value;
+      // if(!validateEmail(email)) {
+      // 	alert('bad email');
+      // 	return false;
+      // }else{
     allNextBtn.click(function(){
       var curStep = $(this).closest(".setup-content"),
           curStepBtn = curStep.attr("id"),
           nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
           curInputs = curStep.find("input[type='text'],input[type='url'], input[type='email']"),
-          isValid = true,
-          email = curInputs[6].value;
-      if(!validateEmail(email)) {
-      	alert('bad email');
-      	return false;
-      }else{
+          isValid = true;
+
         $(".form-group").removeClass("has-error");
         for(var i=0; i<curInputs.length; i++){
             if (!curInputs[i].validity.valid){
@@ -47,7 +47,7 @@ jQuery(document).ready(function($) {
                 $(curInputs[i]).closest(".form-group").addClass("has-error");
             }
         }
-      }
+      
         if (isValid)
             nextStepWizard.removeAttr('disabled').trigger('click');
     });
