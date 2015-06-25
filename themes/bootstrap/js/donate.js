70158e615cc0b80b420eb7b30a3ba08b52ef6d0f
Kyle <kyle.warbis@gmail.com>
2015-06-24 17:32:59 -0700

    add audio to press page

--- themes/bootstrap/js/donate.js
+++ themes/bootstrap/js/donate.js
@@ -4,10 +4,6 @@
 
 
 jQuery(document).ready(function($) {
-		function validateEmail(email) {
-			var validator = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-			return validator.test(email);
-		}
     var navListItems = $('div.setup-panel div a'),
         allWells = $('.setup-content'),
         allNextBtn = $('.nextBtn');
@@ -28,11 +24,7 @@ jQuery(document).ready(function($) {
             $target.find('input:eq(0)').focus();
         }
     });
-      // email = curInputs[6].value;
-      // if(!validateEmail(email)) {
-      // 	alert('bad email');
-      // 	return false;
-      // }else{
+
     allNextBtn.click(function(){
       var curStep = $(this).closest(".setup-content"),
           curStepBtn = curStep.attr("id"),
