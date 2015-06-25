fe23a6f19cba3feac92c9d451c9431367a3c5a8d
komar <komar@upwardstech.com>
2015-06-25 18:48:16 +0700

    tags home

--- themes/bootstrap/js/tags.js
+++ themes/bootstrap/js/tags.js
@@ -39,6 +39,9 @@ jQuery(document).ready(function($) {
             tags +='var axel = Math.random() + "";';
             tags +='var a = axel * 10000000000000;';
             tags +="document.write('<iframe src=\"https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=home0;ord=' + a + '?\" width=\"1\" height=\"1\" frameborder=\"0\" style=\"display:none\"></iframe>');";
+            tags +="<noscript>";
+            tags += "<iframe src=\"https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=home0;ord=1?\" width=\"1\" height=\"1\" frameborder=\"0\" style=\"display:none\"></iframe>";
+            tags +="</noscript>";
             tags +=' </script>';
         $( "#tags" ).append( tags );
 
