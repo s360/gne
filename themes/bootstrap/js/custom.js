e4b31eb1a7e49d528a833b20a3ae0efcf564cce4
Kyle <kyle.warbis@gmail.com>
2015-06-25 15:36:57 -0700

    merge

--- themes/bootstrap/js/custom.js
+++ themes/bootstrap/js/custom.js
@@ -358,7 +358,7 @@ jQuery(document).ready(function() {
 			return validator.test(email);
 		}
 		var donateJoinButtons = jQuery('.donate-join');
-		donatejoinButtons.click('#donation-overlay-container').removeClass('hidden');
+		donateJoinButtons.click('#donation-overlay-container').removeClass('hidden');
 		var hideOnSuccess = jQuery('#join, .petition-overlay-container, .add-voice, .petition-form-container-story, .story-join');
 		var showOnSuccess = jQuery('.donate-join, .story-share, .add-donate, .thank-arrows');
 
