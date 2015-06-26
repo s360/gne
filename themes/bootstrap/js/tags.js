99a0cc48ab0ccb8f419b7109b2f144495f24a3bf
komar <komar@upwardstech.com>
2015-06-26 11:54:30 +0700

    Revert "tags js"
    This reverts commit 542bae2b5473f4dd5728eede6c0b0480560d17ed.

--- themes/bootstrap/js/tags.js
+++ themes/bootstrap/js/tags.js
@@ -2,64 +2,50 @@
  * Created by Upwardstech on 6/24/15.
  */
 
-/*  function gotohash(e) {
-    var lochash = window.location.hash;
-    *//*var hurl = window.location.host ;
-    var axel = Math.random() + "";
-    var a = axel * 10000000000000;*//*
-
-}
-
-jQuery( document ).ready( gotohash );
-jQuery( window ).bind( 'hashchange', gotohash );*/
-
-var lochash = window.location.hash;
-
-function dc_tracker(cat_name) {
-    var axel = Math.random() + "";
-    var a = axel * 10000000000000;
-   // document.write("<iframe src=\"https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat="+cat_name+";ord=" + a + "?\" width=\"1\" height=\"1\" frameborder=\"0\" style=\"display:none\"><\/iframe>");
-    iframe = jQuery('<iframe src="https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat='+cat_name+';ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none">');
-    iframe.appendTo('body');
-}
+  /*  function gotohash(e) {
+        var lochash = window.location.hash;
+        *//*var hurl = window.location.host ;
+        var axel = Math.random() + "";
+        var a = axel * 10000000000000;*//*
 
 
-if(location.pathname == "/" && lochash == '') {
-    dc_tracker('home0');
-}
-/*if(lochash == "#!/donate" ) {
-    dc_tracker('donate0');
-}*/
-
-var donateButton   	 			 = document.getElementById('donate-nav');
-var overlay  	 			 = document.getElementById('overlay-submit');
-
-/*
-donateButton.addEventListener('click', function() {
-    dc_tracker('donate0');
-    console.log('test')
-});
-*/
-
-/*jQuery( document ).ready(function(){
-    jQuery("#donate-nav").click( function() { dc_tracker('involve0') });
-});*/
-jQuery( document ).ready(function() {
-    //jQuery("#overlay-submit").click( function() { dc_tracker('join0') });
-    jQuery("#donate-nav").click( function() { dc_tracker('donate0') });
-    jQuery("input.zip_sub").click( function() { dc_tracker('involve0') });
-    jQuery(".petition-submit").click( function() { dc_tracker('join0') });
-    jQuery("#social-footer").attr("facebook").click( function() { dc_tracker('share_fb') });
-    jQuery("#social-footer").attr("Twitter").click( function() { dc_tracker('share_tw') });
-    /*
-    function gotohash(e) {
-        if(lochash == "#!/donate" ) {
-            dc_tracker('donate0');
-        }
 
     }
+
     jQuery( document ).ready( gotohash );
     jQuery( window ).bind( 'hashchange', gotohash );*/
+var lochash = window.location.hash;
+    var axel = Math.random() + "";
+    var a = axel * 10000000000000;
+    var cat = '';
+    var iframe = null;
+if(location.pathname == "/" && lochash == '') {
+    iframe = jQuery('<iframe src="https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=home0;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none">');
+    iframe.appendTo('body');
+//    document.body.insertAdjacentHTML('afterbegin', '<script type="text/javascript">'
+//        + 'var axel = Math.random() + "";'
+//        + 'var a = axel * 10000000000000;'
+//        + 'document.write(\'<iframe src="https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=error404;ord=\' + a + \'?" width="1" height="1" frameborder="0" style="display:none"><\/iframe>\');'
+//        + '<\/script>'
+//        + '<noscript>'
+//        + '<iframe src="https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=error404;ord=1?" width="1" height="1" frameborder="0" style="display:none"><\/iframe>'
+//        + '<\/noscript>');
+    // document.write("<iframe src=\"https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=home0;ord=' + a + '?\" width=\"1\" height=\"1\" frameborder=\"0\" style=\"display:none\"><\/iframe>");
+}
+
+if(lochash == "#!/donate" ) {
+    iframe.remove();
+    iframe = jQuery('<iframe src="https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=donate0;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none">');
+    iframe.appendTo('body');
+
+}
+
+jQuery(function(){
+    jQuery('#overlay-submit').bind('click', function(){
+
+    });
 });
 
+
+//jQuery('<iframe src="https://4652923.fls.doubleclick.net/activityi;src=4652923;type=2015_0;cat=home0;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none">').appendTo('body');
 
