b0e9559f6b5a48b946fe0cf9855c5a483f113bba
Kyle <kyle.warbis@gmail.com>
2015-06-25 19:32:20 -0700

    close navbar when clicked off

--- themes/bootstrap/js/custom.js
+++ themes/bootstrap/js/custom.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function() {
 			if(!jQuery("#petition-overlay-container").hasClass('hidden') && jQuery(e.target).hasClass('full_overlay')) {
     		jQuery("#petition-overlay-container").addClass('hidden');
     	}
-    	if(jQuery(".top-navigation").hasClass('in') && !jQuery(e.target).hasClass('nav-toggle')) {
+    	if(jQuery('.top-navigation').hasClass('in') && !jQuery(e.target).hasClass('navbar-toggle') && !jQuery(e.target).hasClass('icon-bar')) {
     		jQuery('.top-navigation').removeClass('in');
     	}
 		});
