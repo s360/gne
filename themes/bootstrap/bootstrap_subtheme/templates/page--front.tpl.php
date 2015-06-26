d1a53716cf34a3a242218728ed603bd71b0600e3
Abraham Godong <abe@godong.or.id>
2015-06-25 19:37:47 -0700

    Update Drupal Title

--- themes/bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
+++ themes/bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
@@ -75,7 +75,7 @@
  */
 ?>
 <?php
-drupal_set_title("Great Nations Eat - Unite and Fight To End Hunger");
+drupal_set_title("Unite and Fight To End Hunger");
 drupal_add_css($directory.'/js/slick/slick.css', array('group' => CSS_THEME, 'every_page' => FALSE));
 drupal_add_css($directory.'/js/slick/slick-theme.css', array('group' => CSS_THEME, 'every_page' => FALSE));
 ?>
