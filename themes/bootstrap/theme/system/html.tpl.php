45da8265c39e7ba979f0e56aa27410fd7c954c2d
Kyle <kyle.warbis@gmail.com>
2015-07-07 14:31:07 -0700

    fix thankyou page

--- themes/bootstrap/theme/system/html.tpl.php
+++ themes/bootstrap/theme/system/html.tpl.php
@@ -66,11 +66,11 @@
 
     <!-- fonts -->
     <link href='//fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <link href='/sites/all/fonts/fonts.css' rel='stylesheet' type='text/css'>
     <link href='//fonts.googleapis.com/css?family=Open+Sans:700,600,400,300' rel='stylesheet' type='text/css'>
     <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
     <link href='//fonts.googleapis.com/css?family=Ultra' rel='stylesheet' type='text/css'>
 		<link href='//fonts.googleapis.com/css?family=Playfair+Display:700italic' rel='stylesheet' type='text/css'>
+		<link href='/sites/all/fonts/fonts.css' rel='stylesheet' type='text/css'>
 
 
     <!--[if IE]>
