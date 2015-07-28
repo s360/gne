28ccd5558a82e42b4f2c11af673de6f33f9ca393
komar <komar@upwardstech.com>
2015-07-28 15:54:05 +0700

    donation form 3

--- themes/bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
+++ themes/bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
@@ -511,7 +511,7 @@ drupal_add_css($directory.'/js/slick/slick-theme.css', array('group' => CSS_THEM
 </div>
 <?php if (!empty($page['donation_form'])): ?>
     <div id="block-donation-form-overlay">
-        <?php print render($page['donation_form']); ?>
+        <?php //print render($page['donation_form']); ?>
     </div>
 <?php endif; ?>
 <footer id="footer">
