3b2995640be9ebdb226d2c335a9cd04d0982923b
Abraham Godong <abe@godong.or.id>
2015-07-28 23:03:37 -0700

    Add height to the homepage background

--- themes/bootstrap/bootstrap_subtheme/templates/page--donation_form.tpl.php
+++ themes/bootstrap/bootstrap_subtheme/templates/page--donation_form.tpl.php
@@ -166,6 +166,7 @@ drupal_add_css($directory.'/js/slick/slick-theme.css', array('group' => CSS_THEM
 </div>
 <script type="text/javascript">
     jQuery(document).ready(function() {
+        jQuery('#stories').height(jQuery(window).height());
         jQuery('.leaf.visible-xs.join').click(function() {
             jQuery('#petition-overlay-container').removeClass('hidden');
         });
