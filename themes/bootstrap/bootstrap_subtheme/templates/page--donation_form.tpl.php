0cd993cdbebaa3452d0c5b8607577b46905b9bd4
Abraham Godong <abe@godong.or.id>
2015-07-28 22:58:18 -0700

    Add homepage as background

--- themes/bootstrap/bootstrap_subtheme/templates/page--donation_form.tpl.php
+++ themes/bootstrap/bootstrap_subtheme/templates/page--donation_form.tpl.php
@@ -115,7 +115,7 @@ drupal_add_css($directory.'/js/slick/slick-theme.css', array('group' => CSS_THEM
         </div>
     </header>
     <div class="main-container">
-        <div id="thestories">
+        <div id="stories">
             <div class="page1-container">
                 <div class="story_pics">
                     <div class="col-sm-4 col-xs-4 onethird one">
