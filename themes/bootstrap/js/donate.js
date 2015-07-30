56562d8c99f066236d816eb94eebe4ffe59e1401
Abraham Godong <abe@godong.or.id>
2015-07-29 17:02:11 -0700

    troubleshoot IOS JS issue

--- themes/bootstrap/js/donate.js
+++ themes/bootstrap/js/donate.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function($) {
             nextStepWizard.removeAttr('disabled').trigger('click');
     });
     console.log("Trigger click on donate");
-    jQuery("#step-one").trigger('click');
+    //jQuery("#step-one").trigger('click');
     if( window.location.hash == '#!/donate'){
         donate();
     }
